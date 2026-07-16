c65d2c99a382985ad19f6cab358ce8a0dec104f5
Jorge Guillen <jguillen@externos.bancoestado.cl>
2026-07-16 17:10:26 -0500

    :wrench: feat(docs): generar componentes base para el layout de la documentación
    - Crear componentes standalone: Topbar, Sidebar, Footer y AppLayout bajo core/layout
    - Configurar el andamiaje HTML y estilos SCSS iniciales de AppLayoutComponent
    - Importar los nuevos componentes dentro de AppLayout para el ensamblado del cascarón visual

---
+++ projects/docs/src/app/pages/playground/playground.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playground',
+  imports: [],
+  templateUrl: './playground.html',
+  styleUrl: './playground.scss',
+})
+export class Playground {}
